03169943937e26872de4f573487069a26a0e3f8c
Ciunas <ciunas.bennett@gmail.com>
2017-07-21 13:31:29 +0100

    secong stage added

--- src/application/FXController.java
+++ src/application/FXController.java
@@ -2,14 +2,22 @@ package application;
 
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.File;
+import java.io.IOException;
 
-public class FXController  {
+public class FXController {
 
     @FXML
     private TextField text1;
@@ -23,10 +31,22 @@ public class FXController  {
         File selectedFile = fileChooser.showOpenDialog(null);
         if (selectedFile != null) {
 
-            text1.setText("File selected: " + selectedFile.getName());
-        }
-        else {
+            text1.setText(selectedFile.getName());
+        } else {
             text1.setText("File selection cancelled.");
         }
+    }
+
+    @FXML
+    public void newScene(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("inputStage.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("ABC");
+        stage.setScene(new Scene(root1));
+        stage.show();
+
     }
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 800));
+        primaryStage.setTitle("Text Encryption Application");
+        primaryStage.setScene(new Scene(root, 500, 250));
         primaryStage.show();
     }
 
