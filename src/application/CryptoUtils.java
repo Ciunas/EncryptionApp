d024586c3b510369ab401ff2be3c4eb8310bc958
Ciunas <ciunas.bennett@gmail.com>
2017-07-22 21:48:45 +0100

    update text are encode

--- src/application/CryptoUtils.java
+++ src/application/CryptoUtils.java
@@ -1,12 +1,15 @@
 package application;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -26,6 +29,16 @@ public class CryptoUtils {
     public static void decrypt(String key, File inputFile, File outputFile)
             throws CryptoException {
         doCrypto(Cipher.DECRYPT_MODE, key, inputFile, outputFile);
+    }
+
+    public static byte[] encryptText(String key, String inputText)
+            throws CryptoException {
+        return doCryptoText(Cipher.ENCRYPT_MODE, key, inputText);
+    }
+
+    public static byte[] decryptText(String key, String inputText)
+            throws CryptoException {
+        return doCryptoText(Cipher.DECRYPT_MODE, key, inputText);
     }
 
     private static void doCrypto(int cipherMode, String key, File inputFile,
@@ -53,4 +66,39 @@ public class CryptoUtils {
             throw new CryptoException("Error encrypting/decrypting file", ex);
         }
     }
+
+    private static byte [] doCryptoText(int cipherMode, String key, String inputString) throws CryptoException {
+        try {
+            Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(cipherMode, secretKey);
+
+            //FileInputStream inputStream = new FileInputStream(inputFile);
+            System.out.println(inputString + " inputString");
+            //byte[] inputBytes = new byte[ inputString.length() ];
+            byte[] inputBytes = Base64.getEncoder().encode( inputString.getBytes() );
+
+            String string1 = new String(Base64.getDecoder().decode(inputBytes));
+            System.out.println(string1 + " inputString");
+            //byte[] inputBytes = new byte[ (int) inputString.length()];
+                    //new byte[(int) inputString.length()];
+            //inputStream.read(inputBytes);
+
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+
+            //FileOutputStream outputStream = new FileOutputStream(outputFile);
+            //outputStream.write(outputBytes);
+
+            //inputStream.close();
+            //outputStream.close();
+            return outputBytes;
+
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException ex) {
+            throw new CryptoException("Error encrypting/decrypting file", ex);
+        }
+    }
+
+
 }
--- src/application/InputStage.java
+++ src/application/InputStage.java
@@ -2,17 +2,55 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
-import java.awt.*;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
 
 public class InputStage {
 
     @FXML
-    TextField textField1;
+    private TextArea textArea1;
+
+    @FXML
+    private TextArea textArea2;
 
     public void encryptMessage(ActionEvent actionEvent) {
 
-        String text = textField1.getText();
+        //textArea2.setText("");
+        String text = textArea1.getText();
+        System.out.println(text);
+
+        String key = "Mary has one cat";
+
+        try {
+            textArea2.setText( new String( CryptoUtils.encryptText(key,  textArea1.getText() )));
+
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        textArea1.setText("");
+    }
+
+    public void decryptMessage(ActionEvent actionEvent) {
+
+        String text = textArea1.getText();
+        System.out.println(text);
+
+        String key = "Mary has one cat";
+
+        try {
+
+            textArea2.setText( new String( CryptoUtils.decryptText(key, textArea1.getText())));
+
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        textArea1.setText("");
     }
 
 }
